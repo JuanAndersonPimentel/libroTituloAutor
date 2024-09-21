d91be893ab87968bc6e51371bbd61198156ca5ac
UserGPC <UserGPC@DESKTOP-4VVRAQ5>
2024-09-21 14:30:40 -0400

    Agregar archivos de proyecto.

---
+++ libroTituloAutor/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace libroTituloAutor
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            libro libro = new libro();
+
+            try
+            {
+                Console.WriteLine("ingrese el titulo del libro:");
+                libro.titulo = Console.ReadLine();
+
+                Console.WriteLine("ingrese el autor del libro:");
+                libro.autor = Console.ReadLine();
+
+                Console.WriteLine("ingrese el nuemro de paginas del libro:");
+                libro.paginas = int.Parse(Console.ReadLine());
+
+                libro.mostrarinformacion();
+
+                if (libro.eslargo())
+                {
+                    Console.WriteLine("el libro es largo (mas de 500 paginas).");
+                }
+                else 
+                {
+                    Console.WriteLine("el libro no es largo.");
+                }
+
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("error: el valor ingresado no es valido. ingrese un numero entero para las paginas");
+                
+            }
+        }
+    }
+}
---
+++ libroTituloAutor/libro.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace libroTituloAutor
+{
+    public class libro
+    {
+        public string titulo { get; set; }
+        public string autor { get; set; }
+        public int paginas { get; set; }
+
+        public void mostrarinformacion()
+        {
+            Console.WriteLine($"titulo: {titulo}");
+            Console.WriteLine($"autor: {autor}");
+            Console.WriteLine($"paginas: {paginas}");
+        }
+        public bool eslargo()
+        {
+            return paginas > 500;
+        }
+
+        
+        
+    }
+    
+
+}
+
